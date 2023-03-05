159f3ab640a562423d615d594540a88dc79e8a1e
Lucas Spitzer <spitzerl@tuta.io>
2023-03-05 18:26:25 +0100

    Modification du manifset.json

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = `temperature-converter-v1`;
+const CACHE_NAME = `sparkless`;
 
 // Use the install event to pre-cache all initial resources.
 self.addEventListener("install", (event) => {
 	event.waitUntil(
 		(async () => {
 			const cache = await caches.open(CACHE_NAME);
-			cache.addAll(["/", "/converter.js", "/converter.css"]);
+			cache.addAll(["/"]);
 		})()
 	);
 });
